b97b97115f3aef1d370bdac6a0ea4c1059ab27d9
xivv <saverio_mancuso@gmx.de>
2016-07-11 12:36:50 +0200

    Added MatchURL

--- data/StringPattern.java
+++ data/StringPattern.java
@@ -11,4 +11,5 @@ public class StringPattern {
 	public static Pattern teamPercentPattern = Pattern.compile("<i>(.*?)</i>");
 	public static Pattern teamWonPattern = Pattern.compile("csgolounge.com/img/won.png");
 	public static Pattern commentPattern = Pattern.compile("#D12121\">(.*?)</span>");
+	public static Pattern matchURLPattern = Pattern.compile("m=(.*?)\"");
 }
